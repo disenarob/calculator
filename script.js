d7983321a16de13ee9f52951c9c6171d5bbf90d9
Diseanrob <mail@disena.no>
2022-03-11 15:06:26 +0100

    changed layout and css

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const calculate = {
+    add: function (num1, num2) {
+        return num1 + num2;
+    },
+
+    subtract: function (num1, num2) {
+        return num1 - num2;
+    },
+
+    multiply: function (num1, num2) {
+        return num1 * num2;
+    },
+
+    divide: function (num1, num2) {
+        return num1 / num2;
+    }
+
+};
+
+
+document.onload = console.log(calculate.divide(1,2));
